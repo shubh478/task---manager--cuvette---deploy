f2ef59e3efcdc5d7a624a223deec5ebcba2c6875
Shubham <shubhamkumar478@gmail.com>
2024-03-02 08:22:18 +0530

    url added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy:{'/api':"http://localhost:5070/"}
+    proxy: { "/api": "https://task-manager-cuvette-deploy-b7m1.vercel.app/" },
   },
   plugins: [react()],
-})
+});
